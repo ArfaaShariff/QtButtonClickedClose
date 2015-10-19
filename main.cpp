88ea38c5a99161ecb388177d300ff82c9123668a
Arfaa Nisar Shariff <arfaa@plank.life>
2015-10-19 14:59:29 +0530

    QDir: checking if a directory exists

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
+    #include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QDir mDir("C:/QtProjects") ;
+    qDebug()<<mDir.exists();
     MainWindow w;
     w.show();
 
