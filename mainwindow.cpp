a243c7829de429710643d8c75ec991075ae637ee
Arfaa Nisar Shariff <arfaa@plank.life>
2015-10-19 12:55:21 +0530

    Opened a dialogBox, previous window  does not provide functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "mydialog.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setCentralWidget(ui->plainTextEdit);
     ui->pushButton->setText("Close");
 
     connect(ui->horizontalSlider,SIGNAL(valueChanged(int)),
@@ -22,4 +24,15 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionNew_Window_triggered()
+{
+//   MyDialog mDialog;
+//    mDialog.setModal(true);
+//    mDialog.exec();
+//    mDialog.show();
+    mDialog = new  MyDialog(this);
+    mDialog->show();
+
 }
